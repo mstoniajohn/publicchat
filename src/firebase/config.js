700c90efa8b6159b2b3271cf93bdc8c8f3fba7e6
github username <github email address>
2021-03-11 11:06:30 -0500

    changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import 'firebase/database';
 import 'firebase/storage';
 
 const firebaseConfig = {
-	apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+	apiKey: 'AIzaSyC7DrrP16MBjD3hvU9on1qRshqYUj8FdO8',
 	authDomain: 'vue-udemy-cc1b7.firebaseapp.com',
 	databaseURL: 'https://vue-udemy-cc1b7-default-rtdb.firebaseio.com',
 	projectId: 'vue-udemy-cc1b7',
 	storageBucket: 'vue-udemy-cc1b7.appspot.com',
 	messagingSenderId: '14672674618',
-	appId: process.env.VUE_APP_FIREBASE_APP_ID,
+	appId: '1:14672674618:web:9e2c32c9cd2d4a9255da03',
 };
 // initilize appa
 firebase.initializeApp(firebaseConfig);
